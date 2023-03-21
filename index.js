321d1a4fb675dad334f78354da44a2e537bfe9cc
JaniAnttonen <jani.anttonen@protonmail.ch>
2023-03-21 16:48:54 +0200

    Ebin

--- index.js
+++ index.js
@@ -1,75 +1,31 @@
-import { Router } from 'itty-router';
+import { Client } from "@notionhq/client";
+import { Router } from "itty-router";
+import { NotionToMarkdown } from "notion-to-md";
 
-// Create a new router
 const router = Router();
 
-/*
-Our index route, a simple hello world.
-*/
-router.get('/', () => {
-	return new Response('Hello, world! This is the root page of your Worker template.');
+router.get('/', async () => {
+	const notion = new Client({
+		auth: NOTION_TOKEN,
+	})
+
+	const n2m = new NotionToMarkdown({ notionClient: notion });
+	const db = await notion.databases.query({
+		database_id: DATABASE_ID,
+	})
+  return new Response(JSON.stringify(db))
 });
 
-/*
-This route demonstrates path parameters, allowing you to extract fragments from the request
-URL.
+router.get('/:id', async ({ params }) => {
+	const notion = new Client({
+		auth: NOTION_TOKEN,
+	})
 
-Try visit /example/hello and see the response.
-*/
-router.get('/example/:text', ({ params }) => {
-	// Decode text like "Hello%20world" into "Hello world"
-	let input = decodeURIComponent(params.text);
-
-	// Serialise the input into a base64 string
-	let base64 = btoa(input);
-
-	// Return the HTML with the string to the client
-	return new Response(`<p>Base64 encoding: <code>${base64}</code></p>`, {
-		headers: {
-			'Content-Type': 'text/html',
-		},
-	});
+	const n2m = new NotionToMarkdown({ notionClient: notion });
+	const blocks = await n2m.pageToMarkdown(params.id);
+  const page = n2m.toMarkdownString(blocks);
+	return new Response(JSON.stringify(page));
 });
-
-/*
-This shows a different HTTP method, a POST.
-
-Try send a POST request using curl or another tool.
-
-Try the below curl command to send JSON:
-
-$ curl -X POST <worker> -H "Content-Type: application/json" -d '{"abc": "def"}'
-*/
-router.post('/post', async request => {
-	// Create a base object with some fields.
-	let fields = {
-		asn: request.cf.asn,
-		colo: request.cf.colo,
-	};
-
-	// If the POST data is JSON then attach it to our response.
-	if (request.headers.get('Content-Type') === 'application/json') {
-		let json = await request.json();
-		Object.assign(fields, { json });
-	}
-
-	// Serialise the JSON to a string.
-	const returnData = JSON.stringify(fields, null, 2);
-
-	return new Response(returnData, {
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	});
-});
-
-/*
-This is the last route we define, it will match anything that hasn't hit a route we've defined
-above, therefore it's useful as a 404 (and avoids us hitting worker exceptions, so make sure to include it!).
-
-Visit any page that doesn't exist (e.g. /foobar) to see it in action.
-*/
-router.all('*', () => new Response('404, not found!', { status: 404 }));
 
 export default {
 	fetch: router.handle,
